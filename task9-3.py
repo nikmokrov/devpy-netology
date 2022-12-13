48fcb1962bd7e28b657fae6be8b2df6857efcc3c
Nikolay Mokrov <nikmokrov@yandex.ru>
2022-12-13 15:58:56 +0300

    Task 9.3 done

--- task9-3.py
+++ task9-3.py
@@ -1,20 +1,24 @@
 import requests
 from datetime import datetime, timedelta
 
-fromdate = int((datetime.today() - timedelta(days=1)).timestamp())
+fromdate = int((datetime.today() - timedelta(days=2)).timestamp())
 todate = int((datetime.today()).timestamp())
 has_more = True
 page = 1
 news = []
 while has_more:
-    url = f"https://api.stackexchange.com/2.3/questions?page={page}&pagesize=25&order=asc&\
-            fromdate={fromdate}&todate={todate}&sort=creation&tagged=python&site=stackoverflow"
-    # print(url)
+    url = f"https://api.stackexchange.com/2.3/questions?page={page}&pagesize=20&order=asc\
+    &fromdate={fromdate}&todate={todate}&sort=creation&tagged=python&site=stackoverflow"
     req = requests.get(url)
+    if req.status_code != 200:
+        break
     items = req.json()['items']
     for item in items:
-        news.append({datetime.fromtimestamp(int(item['creation_date'])): item['title']})
+        news.append((datetime.fromtimestamp(int(item['creation_date'])), item['title']))
     has_more = bool(req.json()['has_more'])
-    print(bool(req.json()['has_more']))
     page += 1
-print(news)
+
+print('All news tagged with Python in the last 2 days:')
+for item in news:
+    print(item[0], item[1])
+print(f'Total pages: {page-1}')
